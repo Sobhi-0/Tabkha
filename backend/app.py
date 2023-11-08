8f60aa4b8053a08a676d35d1d068ec949f99bfdb
Sobhi <s@>
2023-11-09 01:11:16 +0300

    fix the database relations

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,9 @@
-from database.models import db_drop_and_create_all, setup_db
-from flask import Flask, flash, redirect, render_template, request, session
+import datetime
+
+from database.models import (Category, Instruction, Recipe, User, db,
+                             db_drop_and_create_all, setup_db)
+from flask import (Flask, flash, jsonify, redirect, render_template, request,
+                   session)
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -7,14 +11,44 @@ from werkzeug.security import check_password_hash, generate_password_hash
 def create_app(db_URI="", test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 
     if db_URI:
         setup_db(app, db_URI)
     else:
         setup_db(app)  
 
+    # with app.app_context():
+    #     db_drop_and_create_all()
 
-    # # configure session to use filesystem (instead of signed cookies)
+    # print("lol1")
+    # Create users and add them to the session
+    # user1 = User(first_name="John", last_name="Doe", email="john@example.com", password_hash="hashed_password1", created_at=datetime.datetime.now())
+    # user2 = User(first_name="Jane", last_name="Smith", email="jane@example.com", password_hash="hashed_password2", created_at=datetime.datetime.now())
+
+    # Create recipes and add them to the session
+    # recipe1 = Recipe(title="Recipe 1", description="Description 1", prepare_time=30, cook_time=60, user_id=1, created_at=datetime.datetime.now())
+    # recipe2 = Recipe(title="Recipe 2", description="Description 2", prepare_time=20, cook_time=45, user_id=2, created_at=datetime.datetime.now())
+
+    # Create instructions and add them to the session
+    # instruction1 = Instruction(step_number=1, description="Step 1 for Recipe 1", recipe_id=1)
+    # instruction2 = Instruction(step_number=2, description="Step 2 for Recipe 1", recipe_id=1)
+    # instruction3 = Instruction(step_number=1, description="Step 1 for Recipe 2", recipe_id=2)
+
+    # # Create categories and add them to the session
+    # category1 = Category(category="Category 1")
+    # category2 = Category(category="Category 2")
+
+    # Add data to the database session and commit the changes
+    # with app.app_context():
+    #     instruction1.insert()
+    #     instruction2.insert()
+    #     instruction3.insert()
+    #     category1.insert()
+    #     category2.insert()
+
+
+    # configure session to use filesystem (instead of signed cookies)
     # app.config["SESSION_PERMANENT"] = False
     # app.config["SESSION_TYPE"] = "filesystem"
     Session(app)
@@ -23,6 +57,21 @@ def create_app(db_URI="", test_config=None):
     @app.route('/health')
     def health():
         return "Up and Running!"
+
+    
+    @app.route("/")
+    def index():
+        recipes = Recipe.query.order_by(Recipe.id.desc()).limit(5).all()
+
+        dict = {}
+
+        # for i in recipes:
+
+
+        print("HERE ==>\n", recipes)
+
+        return render_template("home.html", recipes=recipes)
+        
 
 
     return app
--- backend/database/models.py
+++ backend/database/models.py
@@ -11,7 +11,7 @@ database_path = os.environ.get('DATABASE_URL')
 db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres@localhost:5432/recipe_sharing"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     migrate = Migrate(app, db)
@@ -23,11 +23,6 @@ def setup_db(app, database_path=database_path):
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
-
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
 
 
 class BaseModel(db.Model):
@@ -46,6 +41,23 @@ class BaseModel(db.Model):
         db.session.commit()
 
 
+favorites = db.Table(
+    "favorites",
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("recipe_id", db.Integer, db.ForeignKey("recipes.id"), primary_key=True),
+    db.Column("created_at", db.DateTime, nullable=False)
+)
+
+
+follows = db.Table(
+    "follows",
+    db.Column("following_user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("followed_user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
+    db.Column("created_at", db.DateTime, nullable=False),
+    info={'foreign_keys': ['follows.following_user_id', 'follows.followed_user_id']}
+)
+
+
 class User(BaseModel):
     __tablename__ = "users"
 
@@ -55,14 +67,18 @@ class User(BaseModel):
     email = db.Column(db.String, unique=True, nullable=False)
     image = db.Column(db.LargeBinary)
     password_hash = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
 
-    # Establish a many-to-many relationship between models
-    favorite_recipes = db.relationship('Recipe', secondary='favorite_recipes', back_populates='favorited_by')
-    favorite_recipes = db.relationship('Follower', secondary='followers', back_populates='followers')
+    # one-to-many relations
+    recipes = db.relationship('Recipe', backref='users', lazy=True)
 
-# new_recipe = Recipe(title="Recipe Title", description="Recipe Description")
-# instruction1 = Instruction(step_number=1, description="Step 1 Description", recipe=new_recipe)
-# instruction2 = Instruction(step_number=2, description="Step 2 Description", recipe=new_recipe)
+    # many-to-many relations
+    favorites = db.relationship('Recipe', secondary=favorites, lazy='subquery', backref=db.backref('favorite', lazy=True))
+    # follows = db.relationship('User', secondary=follows, lazy='subquery', backref=db.backref('follows', lazy=True))
+    follows = db.relationship('User', secondary=follows, primaryjoin=(follows.c.following_user_id == id), secondaryjoin=(follows.c.followed_user_id == id), backref=db.backref('followers', lazy=True))
+
+
+
 class Recipe(BaseModel):
     __tablename__ = "recipes"
 
@@ -73,46 +89,23 @@ class Recipe(BaseModel):
     cook_time = db.Column(db.Integer, nullable=False)
     image = db.Column(db.LargeBinary)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
 
-    # Establish a many-to-many relationship between models
-    instructions = db.relationship('Instruction', backref='recipe', lazy=True)
-    categories = db.relationship('Category', secondary='recipe_categories', backref='recipes')
-    favorited_by = db.relationship('User', secondary='favorite_recipes', back_populates='favorite_recipes')
+    # one-to-many relations
+    recipes = db.relationship('Instruction', backref='recipe', lazy=True)
 
 
 class Instruction(BaseModel):
     __tablename__ = "instructions"
 
-    id = db.Column(db.Integer, primary_key=True)
+    # id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), primary_key=True, nullable=False)
     step_number = db.Column(db.Integer, nullable=False)
     description = db.Column(db.Text, nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+
 
 class Category(BaseModel):
     __tablename__ = "categories"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False, unique=True)
-
-# to manage the many-to-many relationship
-recipe_categories = db.Table(
-    'recipe_categories',
-    db.Column('recipe_id', db.Integer, db.ForeignKey('recipes.id')),
-    db.Column('category_id', db.Integer, db.ForeignKey('categories.id'))
-)
-
-
-class FavoriteRecipe(BaseModel):
-    __tablename__ = "favorite_recipes"
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
-
-
-class Follower(BaseModel):
-    __tablename__ = "followers"
-
-    id = db.Column(db.Integer, primary_key=True)
-    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    following_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    category = db.Column(db.String, nullable=False, unique=True)
