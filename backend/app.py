e3cc5cad1a11880a7704542eb4ade5828abd3973
Sobhi-0 <52216865+Sobhi-0@users.noreply.github.com>
2024-02-13 22:53:13 +0300

    Implement /recipes route - Create the recipes page - Move the search bar from home page to recipes page - Redirect popular and recommended recipes to recipes page

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,8 @@ import datetime
 
 from sqlalchemy import text
 
-from database.models import (Category, Instruction, Ingrediant, Recipe, User, db, setup_db)
+from database.models import (
+    Category, Instruction, Ingrediant, Recipe, User, db, setup_db)
 from flask import (Flask, flash, redirect, render_template,
                    request, session)
 from flask_session import Session
@@ -13,7 +14,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 def create_app(db_URI="", test_config=None):
     # create and configure the app
     app = Flask(__name__)
-    app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
+    app = Flask(__name__, template_folder='../frontend/templates',
+                static_folder='../frontend/static')
 
     # Enable template auto-reloading
     app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -21,17 +23,15 @@ def create_app(db_URI="", test_config=None):
     if db_URI:
         setup_db(app, db_URI)
     else:
-        setup_db(app)  
+        setup_db(app)
 
     # with app.app_context():
     #     db_drop_and_create_all()
-
 
     # configure session to use filesystem (instead of signed cookies)
     app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
     Session(app)
-
 
     @app.after_request
     def after_request(response):
@@ -41,25 +41,35 @@ def create_app(db_URI="", test_config=None):
         response.headers["Pragma"] = "no-cache"
         return response
 
-
     @app.route("/health")
     def health():
         return "Up and Running!"
 
-
     @app.route("/my-recipes")
     @login_required
     def my_recipes():
-        recipes = Recipe.query.filter_by(user_id=session["user_id"]).order_by(Recipe.created_at.desc())
-        
+        recipes = Recipe.query.filter_by(
+            user_id=session["user_id"]).order_by(Recipe.created_at.desc())
+
         # Paginating recipes
-        paginated_recipes = paginate_items(selection=recipes, request=request, per_page=7)
+        paginated_recipes = paginate_items(
+            selection=recipes, request=request, per_page=7)
 
         # return jsonify(paginated_recipes)
         return render_template('my-recipes.html', paginated_recipes=paginated_recipes)
 
-
     # Recipe details
+
+    @app.route("/recipes", methods=["GET"])
+    def recipes():
+        # Paginating recipes
+        recipes = Recipe.query.order_by(Recipe.created_at.desc())
+        paginated_recipes = paginate_items(
+            selection=recipes, request=request, per_page=7)
+
+        return render_template('recipes.html', paginated_recipes=paginated_recipes)
+
+
     @app.route("/recipe/<int:recipe_id>", methods=["GET"])
     def recipe(recipe_id):
         recipe = Recipe.query.filter_by(id=recipe_id).first()
@@ -80,20 +90,21 @@ def create_app(db_URI="", test_config=None):
         user = User.query.filter_by(id=recipe.user_id).first()
 
         # Get ingrediants and instructions for the recipe
-        ingrediants_query = text("SELECT * FROM ingrediants WHERE recipe_id = :recipe_id ORDER BY item_number")
+        ingrediants_query = text(
+            "SELECT * FROM ingrediants WHERE recipe_id = :recipe_id ORDER BY item_number")
         parameters = {"recipe_id": recipe_id}
         ingrediants_res = db.session.execute(ingrediants_query, parameters)
         ingrediants_rows = ingrediants_res.fetchall()
         ingrediants = [ingrediant for ingrediant in ingrediants_rows]
-    
-        instructions_query = text("SELECT * FROM instructions WHERE recipe_id = :recipe_id ORDER BY step_number")
+
+        instructions_query = text(
+            "SELECT * FROM instructions WHERE recipe_id = :recipe_id ORDER BY step_number")
         parameters = {"recipe_id": recipe_id}
         instructions_res = db.session.execute(instructions_query, parameters)
         instructions_rows = instructions_res.fetchall()
         instructions = [instruction for instruction in instructions_rows]
 
         return render_template('recipe-details.html', recipe=recipe, ingredients=ingrediants, instructions=instructions, user=user, is_favorite=is_favorite)
-
 
     @app.route("/add-recipe", methods=["GET", "POST"])
     @login_required
@@ -107,7 +118,7 @@ def create_app(db_URI="", test_config=None):
             instructions = request.form.getlist("instructions[]")
             ingrediants = request.form.getlist("ingrediants[]")
 
-            categories = Category.query.all()            
+            categories = Category.query.all()
 
             # Ensure all fields were submitted
             if not title or not description or not prepare_time or not cook_time or not category_id or not instructions or not ingrediants:
@@ -129,16 +140,19 @@ def create_app(db_URI="", test_config=None):
                 return render_template("add-recipe.html", categories=categories, title=title, description=description, prepare_time=prepare_time, cook_time=cook_time, category_id=category_id, instructions=instructions, ingrediants=ingrediants)
 
             # Create a new recipe
-            recipe = Recipe(title=title, description=description, prepare_time=prepare_time, cook_time=cook_time, category_id=category_id, user_id=session["user_id"], created_at=datetime.datetime.now())
+            recipe = Recipe(title=title, description=description, prepare_time=prepare_time, cook_time=cook_time,
+                            category_id=category_id, user_id=session["user_id"], created_at=datetime.datetime.now())
 
             # Add ingrediants to the recipe
             for i, ingrediant in enumerate(ingrediants):
-                recipe.ingrediants.append(Ingrediant(item_number=i+1, item=ingrediant))
+                recipe.ingrediants.append(Ingrediant(
+                    item_number=i+1, item=ingrediant))
 
             # Add instructions to the recipe
             for i, instruction in enumerate(instructions):
-                recipe.instructions.append(Instruction(step_number=i+1, description=instruction))
-            
+                recipe.instructions.append(Instruction(
+                    step_number=i+1, description=instruction))
+
             # Add recipe to the database session and commit the changes
             recipe.insert()
 
@@ -148,7 +162,6 @@ def create_app(db_URI="", test_config=None):
         else:
             categories = Category.query.all()
             return render_template("add-recipe.html", categories=categories)
-
 
     @app.route("/edit-recipe/<int:recipe_id>", methods=["GET", "POST"])
     @login_required
@@ -168,7 +181,7 @@ def create_app(db_URI="", test_config=None):
             instructions = request.form.getlist("instructions[]")
 
             categories = Category.query.all()
-            
+
             # Ensure all fields were submitted
             if not title or not description or not prepare_time or not cook_time or not category_id or not instructions or not ingrediants:
                 flash("Must fill all fields", "warning")
@@ -205,12 +218,14 @@ def create_app(db_URI="", test_config=None):
 
             # Add ingrediants to the recipe
             for i, ingrediant in enumerate(ingrediants):
-                recipe.ingrediants.append(Ingrediant(item_number=i+1, item=ingrediant))
+                recipe.ingrediants.append(Ingrediant(
+                    item_number=i+1, item=ingrediant))
 
             # Add instructions to the recipe
             for i, instruction in enumerate(instructions):
-                recipe.instructions.append(Instruction(step_number=i+1, description=instruction))
-                
+                recipe.instructions.append(Instruction(
+                    step_number=i+1, description=instruction))
+
             recipe.update()
 
             flash("Recipe updated!", "success")
@@ -225,34 +240,38 @@ def create_app(db_URI="", test_config=None):
                 return redirect("/my-recipes")
 
             # Get ingrediants and instructions for the recipe
-            ingrediants_query = text("SELECT * FROM ingrediants WHERE recipe_id = :recipe_id ORDER BY item_number")
+            ingrediants_query = text(
+                "SELECT * FROM ingrediants WHERE recipe_id = :recipe_id ORDER BY item_number")
             parameters = {"recipe_id": recipe_id}
             ingrediants_res = db.session.execute(ingrediants_query, parameters)
             ingrediants_rows = ingrediants_res.fetchall()
             ingrediants = [ingrediant for ingrediant in ingrediants_rows]
 
-            instructions_query = text("SELECT * FROM instructions WHERE recipe_id = :recipe_id ORDER BY step_number")
+            instructions_query = text(
+                "SELECT * FROM instructions WHERE recipe_id = :recipe_id ORDER BY step_number")
             parameters = {"recipe_id": recipe_id}
-            instructions_res = db.session.execute(instructions_query, parameters)
+            instructions_res = db.session.execute(
+                instructions_query, parameters)
             instructions_rows = instructions_res.fetchall()
             instructions = [instruction for instruction in instructions_rows]
 
             return render_template("/edit-recipe.html", recipe=recipe, categories=categories, ingrediants=ingrediants, instructions=instructions)
 
-
     @app.route("/recipe/<int:recipe_id>", methods=["DELETE"])
     @login_required
     def delete_recipe(recipe_id):
         # Paginating recipes
-        recipes = Recipe.query.filter_by(user_id=session["user_id"]).order_by(Recipe.created_at.desc())
-        paginated_recipes = paginate_items(selection=recipes, request=request, per_page=7)
+        recipes = Recipe.query.filter_by(
+            user_id=session["user_id"]).order_by(Recipe.created_at.desc())
+        paginated_recipes = paginate_items(
+            selection=recipes, request=request, per_page=7)
 
         recipe = Recipe.query.filter_by(id=recipe_id).first()
 
         if not recipe:
             flash("Recipe doesn't exist", "error")
             return render_template("my-recipes.html", paginated_recipes=paginated_recipes)
-            
+
         if session["user_id"] != recipe.user_id:
             flash("You are not authorized to delete this recipe", "error")
             return render_template("my-recipes.html", paginated_recipes=paginated_recipes)
@@ -262,19 +281,21 @@ def create_app(db_URI="", test_config=None):
         flash("Recipe deleted!", "success")
         return render_template("my-recipes.html", paginated_recipes=paginated_recipes)
 
-
     # Search for recipes
+
     @app.route("/search", methods=["GET"])
     def search():
         search_query = request.args.get("q")
-        recipes = Recipe.query.filter(Recipe.title.ilike(f"%{search_query}%")).order_by(Recipe.created_at.desc())
+        recipes = Recipe.query.filter(Recipe.title.ilike(
+            f"%{search_query}%")).order_by(Recipe.created_at.desc())
         # Paginating recipes
-        paginated_recipes = paginate_items(selection=recipes, request=request, per_page=7)
+        paginated_recipes = paginate_items(
+            selection=recipes, request=request, per_page=7)
 
         return render_template("search.html", search_query=search_query, paginated_recipes=paginated_recipes)
 
-    
     # Add recipe to favorites
+
     @app.route("/favorite/<int:recipe_id>", methods=["POST"])
     @login_required
     def favorite(recipe_id):
@@ -291,8 +312,8 @@ def create_app(db_URI="", test_config=None):
         flash("Recipe added to favorites!", "success")
         return redirect("/")
 
-
     # Remove recipe from favorites
+
     @app.route("/unfavorite/<int:recipe_id>", methods=["POST"])
     @login_required
     def unfavorite(recipe_id):
@@ -315,8 +336,8 @@ def create_app(db_URI="", test_config=None):
         # return render_template("favorites.html",)
         return redirect("/")
 
-
     # View user's favorites
+
     @app.route("/my-favorites", methods=["GET"])
     @login_required
     def my_favorites():
@@ -324,7 +345,6 @@ def create_app(db_URI="", test_config=None):
         recipes = user.favorites
 
         return render_template("my-favorites.html", recipes=recipes)
-
 
     @app.route("/profile", methods=["GET"])
     @login_required
@@ -336,7 +356,6 @@ def create_app(db_URI="", test_config=None):
             return redirect("/")
 
         return render_template("profile.html", user=user)
-
 
     @app.route("/login", methods=["GET", "POST"])
     def login():
@@ -377,7 +396,6 @@ def create_app(db_URI="", test_config=None):
         else:
             return render_template("login.html")
 
-    
     @app.route("/change-password", methods=["POST"])
     @login_required
     def change_password():
@@ -412,7 +430,6 @@ def create_app(db_URI="", test_config=None):
         flash("Password changed successfully", "success")
         return redirect("/profile")
 
-
     @app.route("/register", methods=["GET", "POST"])
     def register():
         if request.method == "POST":
@@ -424,7 +441,7 @@ def create_app(db_URI="", test_config=None):
             password2 = request.form.get("password2")
 
             # Ensure all fields were submitted
-            if  not firstname or not lastname or not email or not username or not password1 or not password2:
+            if not firstname or not lastname or not email or not username or not password1 or not password2:
                 flash("Must fill all fields", "warning")
                 return render_template("register.html")
 
@@ -446,14 +463,15 @@ def create_app(db_URI="", test_config=None):
             if exist_user:
                 flash("Username already exists", "error")
                 return render_template("register.html", firstname=firstname, lastname=lastname, email=email)
-            
+
             # Ensure email is unique
             exist_email = User.query.filter_by(email=email).first()
             if exist_email:
                 flash("Email already exists", "error")
                 return render_template("register.html", firstname=firstname, lastname=lastname, username=username)
 
-            new_user = User(first_name=firstname, last_name=lastname, username=username, email=email, password_hash=generate_password_hash(password1), created_at=datetime.datetime.now())
+            new_user = User(first_name=firstname, last_name=lastname, username=username, email=email,
+                            password_hash=generate_password_hash(password1), created_at=datetime.datetime.now())
             new_user.insert()
 
             flash("Account created successfully", "success")
@@ -462,7 +480,6 @@ def create_app(db_URI="", test_config=None):
         else:
             return render_template("register.html")
 
-    
     @app.route("/logout")
     def logout():
         # Forget any user_id
@@ -471,16 +488,14 @@ def create_app(db_URI="", test_config=None):
         # Redirect user to login form
         return redirect("/")
 
-    
     @app.route("/")
     def index():
         # Recipes to be displayed on the home page
         recipes = Recipe.query.order_by(Recipe.id.desc()).limit(5).all()
 
         return render_template("home.html", recipes=recipes)
-        
 
     return app
 
 
-app = create_app()
+APP = create_app()
